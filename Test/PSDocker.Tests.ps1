2f0a65979786af67b73ae00767c6e0ad8f961f3d
Steffen Kampmann <s.kampmann@tekaris.com>
2018-05-12 17:41:29 +0200

    Added environment test

--- Test/PSDocker.Tests.ps1
+++ Test/PSDocker.Tests.ps1
@@ -7,19 +7,29 @@ if ( $PSScriptRoot ) { $ScriptRoot = $PSScriptRoot } else { $ScriptRoot = Get-Lo
 $ModuleManifestPath = "$ScriptRoot\..\PSDocker.psd1"
 Import-Module "$ScriptRoot\..\PSDocker.psm1" -Prefix 'Docker' -Force
 
-Describe 'Module Manifest Tests' {
+Describe 'Module Tests' {
 
-    It 'Passes Test-ModuleManifest' {
-        Test-ModuleManifest -Path $ModuleManifestPath | Should Not BeNullOrEmpty
-        $? | Should Be $true
+    Context 'Module' {
+        It 'Passes Test-ModuleManifest' {
+            Test-ModuleManifest -Path $ModuleManifestPath | Should Not BeNullOrEmpty
+            $? | Should Be $true
+        }
     }
-
-    It 'Test docker pull' {
-        Install-DockerImage -Image 'kitematic/hello-world-nginx'
-        try {
-            Install-DockerImage -Image 'foobar'
-            Should Throw
-        } catch {}
+    Context "Docker Service" {
+        It "is running" {
+            $dockerService = Get-Service | Where-Object Name -eq "Docker"
+            $dockerService | Should -Not -BeNullOrEmpty # Docker is not running
+            $dockerService.Status | Should -Be "Running"
+        }
+    }
+    Context 'Cmdlets' {
+        It 'Test docker pull' {
+            Install-DockerImage -Image 'kitematic/hello-world-nginx'
+            try {
+                Install-DockerImage -Image 'foobar'
+                Should Throw
+            } catch {}
+        }
     }
 }
 
