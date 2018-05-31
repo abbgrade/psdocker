a66431a7d88f750abe01b2975b60986f44815c53
Steffen Kampmann <s.kampmann@tekaris.com>
2018-05-31 16:30:34 +0200

    Added timeout to install image

--- Public/Install-Image.ps1
+++ Public/Install-Image.ps1
@@ -3,9 +3,12 @@ function Install-Image {
     param (
         [ValidateNotNullOrEmpty()]
         [string]
-        $Image
+        $Image,
+
+        [int]
+        $TimeoutMS = $null
     )
 
-    Invoke-ClientCommand "pull", $Image
+    Invoke-ClientCommand "pull", $Image -TimeoutMS $TimeoutMS
     Write-Debug "Docker image '$Image' pulled."
 }
