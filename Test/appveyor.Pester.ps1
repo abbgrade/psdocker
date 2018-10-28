9a6c6fe7e3178549878ad413fb883b766afc84fd
Steffen Kampmann <s.kampmann@tekaris.com>
2018-10-28 17:11:11 +0100

    Fixed test call for appveyor

--- Test/appveyor.Pester.ps1
+++ Test/appveyor.Pester.ps1
@@ -3,7 +3,5 @@ $ProjectRoot = $ENV:APPVEYOR_BUILD_FOLDER
 
 Set-Location $ProjectRoot
 
-foreach( $module in @( 'Client', 'Container' )) {
-    Invoke-Pester -Path "$ProjectRoot\src\Modules\$module\Test" -OutputFormat NUnitXml -OutputFile "$ProjectRoot\TestResults.$module.PS$PSVersion.xml" -PassThru |
-        Export-Clixml -Path "$ProjectRoot\PesterResults.$module.$PSVersion.xml"
-}
+Invoke-Pester -Path "$ProjectRoot\src\PSDocker\Test" -OutputFormat NUnitXml -OutputFile "$ProjectRoot\TestResults.PSDocker.PS$PSVersion.xml" -PassThru |
+    Export-Clixml -Path "$ProjectRoot\PesterResults.PSDocker.$PSVersion.xml"
