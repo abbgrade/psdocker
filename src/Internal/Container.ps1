31c69349afc5be6fb5ac65dde5a7f8fdb6ef673b
Steffen Kampmann <s.kampmann@tekaris.com>
2019-02-23 13:03:07 +0100

    Moved types to internal folder

---
+++ src/Internal/Container.ps1
@@ -0,0 +1,21 @@
+class Container {
+    [string] $Id
+    [string[]] $Names
+    [string] $Image
+    [string] $Command
+    [string[]] $LocalVolumes
+    [string[]] $Labels
+    [string[]] $Mounts
+    [string[]] $Networks
+    [string[]] $Ports
+    [string] $CreatedAt
+    [string] $RunningFor
+    [string] $Status
+    [string] $Size
+
+    Container() {
+        $this | Add-Member -Name Name -MemberType ScriptProperty -Value {
+            return $this.Names[0]
+        }
+    }
+}
---
+++ src/Internal/Image.ps1
@@ -0,0 +1,7 @@
+class Image {
+    [string] $Repository
+    [string] $Tag
+    [string] $Id
+    [string] $CreatedAt
+    [string] $Size
+}
