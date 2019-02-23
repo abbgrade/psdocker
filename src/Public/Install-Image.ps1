273c036552ba8c1b38d0b09b8001bf1eed47b414
Steffen Kampmann <s.kampmann@tekaris.com>
2019-02-23 14:07:15 +0100

    Added image output and updated help

--- src/Public/Install-Image.ps1
+++ src/Public/Install-Image.ps1
@@ -3,51 +3,65 @@ function Install-Image {
     <#
 
     .SYNOPSIS
-
     Install image
 
     .Description
-
     Installs a docker image in the service from a repository.
-    Wraps the command [docker pull](https://docs.docker.com/engine/reference/commandline/pull/).
+    Wraps the command `docker pull`.
 
-    .PARAMETER Name
+    .LINK
+    https://docs.docker.com/engine/reference/commandline/pull/
 
+    .PARAMETER Registry
+    Specifies the registry that contains the repository or image.
+
+    .PARAMETER Repository
     Specifies the name of the repository of the image to install.
 
-    .PARAMETER Timeout
+    .PARAMETER Tag
+    Specifies the tag of the image to install.
 
+    .PARAMETER AllTags
+    Specifies if the images for all tags should be installed.
+
+    .PARAMETER Digest
+    Specifies the digest of the specific image that should be installed.
+
+    .PARAMETER Timeout
     Specifies the number of seconds to wait for the command to finish.
 
-    .EXAMPLE
+    .OUTPUTS
+    Image: Returns the images that are installed and match the parameter.
 
+    .EXAMPLE
     PS C:\> Install-DockerImage -Name 'microsoft/nanoserver'
 
     #>
 
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $true)]
+        [Parameter( Mandatory = $false, ValueFromPipelineByPropertyName = $true )]
         [ValidateNotNullOrEmpty()]
         [string] $Registry,
 
-        [Parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true)]
+        [Parameter( Mandatory = $true, ValueFromPipelineByPropertyName = $true )]
         [ValidateNotNullOrEmpty()]
-        [string] $Name,
+        [Alias( 'Name' )]
+        [string] $Repository,
 
-        [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $true)]
+        [Parameter( Mandatory = $false, ValueFromPipelineByPropertyName = $true )]
         [ValidateNotNullOrEmpty()]
         [string] $Tag,
 
-        [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $true)]
+        [Parameter( Mandatory = $false, ValueFromPipelineByPropertyName = $true )]
         [ValidateNotNullOrEmpty()]
         [switch] $AllTags,
 
-        [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $true)]
+        [Parameter( Mandatory = $false, ValueFromPipelineByPropertyName = $true )]
         [ValidateNotNullOrEmpty()]
         [string] $Digest,
 
-        [Parameter(ValueFromPipelineByPropertyName = $true)]
+        [Parameter( ValueFromPipelineByPropertyName = $true )]
         [int] $Timeout = 5 * 60
     )
 
@@ -75,4 +89,6 @@ function Install-Image {
 
     Invoke-ClientCommand "pull", $arguments -Timeout $Timeout
     Write-Verbose "Docker image '$query' pulled."
+
+    Get-Image -Name $query | Write-Output
 }
