87be026c7d4abc16365146d9a8a9c7afb23842d2
Steffen Kampmann <steffen.kampmann@gmail.com>
2020-11-08 13:44:27 +0100

    Fixed windows container tests

--- src/Test/TestHelper.ps1
+++ src/Test/TestHelper.ps1
@@ -10,24 +10,28 @@ $version = Get-DockerVersion
 $testConfig = New-Object -Type PsObject -Property $(
     switch ( $version.Server.Engine.OSArch ) {
         'windows/amd64' {
+
+            $local:image = [ordered] @{
+                Repository = 'mcr.microsoft.com/windows/nanoserver'
+                Tag = [string] ( Get-ComputerInfo -Property 'WindowsVersion' ).WindowsVersion
+            }
+            $local:image.Name = $local:image.Repository + ':' + $local:image.Tag
             @{
-                Image = New-Object PsObject -Property @{
-                    Repository = 'microsoft/nanoserver'
-                    Tag = 'latest'
-                    Name = 'microsoft/nanoserver:latest'
-                }
+                Image = [PSCustomObject] $local:image
                 PrintCommand = 'powershell -c Write-Host'
                 PowershellCommand = 'powershell -c '
                 MountPoint = 'C:\volume'
             } | Write-Output
         }
         'linux/amd64' {
+            $local:image = [ordered] @{
+                Repository = 'microsoft/powershell'
+                Tag = 'latest'
+                Name = 'microsoft/powershell:latest'
+            }
+            $local:image.Name = $local:image.Repository + ':' + $local:image.Tag
             @{
-                Image = New-Object PsObject -Property @{
-                    Repository = 'microsoft/powershell'
-                    Tag = 'latest'
-                    Name = 'microsoft/powershell:latest'
-                }
+                Image = [PSCustomObject] $local:image
                 PrintCommand = 'echo'
                 PowershellCommand = 'pwsh -c '
                 MountPoint = '/tmp/volume'
